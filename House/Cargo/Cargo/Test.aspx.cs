465e2d3c0733ca9374ae09e8a1a24a62aeabb976
Yogi <yogi0397@163.com>
2025-08-20 09:50:16 +0800

    synchronization

--- House/Cargo/Cargo/Test.aspx.cs
+++ House/Cargo/Cargo/Test.aspx.cs
@@ -409,7 +409,7 @@ namespace Cargo
             // RedisHelper.HashSet("HCYCHouseStockSyc", "93_34_LTCT215551603", "LTCT215551603");
             // RedisHelper.HashSet("HCYCHouseStockSyc", "93_34_LTCT225551801", "LTCT225551801");
             CargoInterfaceBus bus = new CargoInterfaceBus();
-
+            return;
             List<CargoProductEntity> testList = bus.QueryNextDayStockSync(new CargoProductEntity { HouseID = 93 });
 
             bus.SaveNextDayProductData(testList);
